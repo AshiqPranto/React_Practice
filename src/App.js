9f790ce4f2d855252d61f8bc3b0d0da0120a6ae7
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-01-12 18:27:10 +0600

    Components part-2 done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {Component} from 'react';
+import {DISHES} from './shared/dishes';
 import Menu from './components/MenuComponent';
 import { Nav, Navbar, NavbarBrand } from 'reactstrap';
 
-function App() {
-  return (
-    <div >
-      <Navbar dark color='primary'> 
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
-}
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//       </div>
-//     );
-//   }
+
+class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+} 
+
+// function App() {
+//   return (
+//     <div >
+//       <Navbar dark color='primary'> 
+//         <div className='container'>
+//           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Menu/>
+//     </div>
+//   );
 // }
 
 export default App;
